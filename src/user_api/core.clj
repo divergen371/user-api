409ded633b7e5f3ea2d763a92226dd7ef3c85b41
divergen371 <51857554+divergen371@users.noreply.github.com>
2025-02-01 16:02:10 +0900

    CRUD追加

--- src/user_api/core.clj
+++ src/user_api/core.clj
@@ -14,19 +14,83 @@
    :body "Hello!! I'm a Clojure API"})
 
 
+(defonce server (atom nil))
+
+(def users (atom {}))
+
+
+(defn create-user
+  [{user :body-params}]
+  (let [id (str (java.util.UUID/randomUUID))
+        new-user (assoc user :id id)]  ; Fixed user creation
+    (swap! users assoc id new-user)    ; Correctly update the users atom
+    {:status 201
+     :body new-user}))                 ; Return the created user
+
+(defn get-users
+  [_]
+  {:status 200
+   :body @users})
+
+
+(defn get-user
+  [{{:keys [id]} :path-params}]
+  (if-let [user (get @users id)]
+    {:status 200
+     :body user}
+    {:status 404
+     :body {:error "User not found"}}))
+
+
+(defn update-user
+  [{{:keys [id]} :path-params user-update :body-params}]
+  (if (get @users id)
+    (let [updated-user (assoc user-update :id id)]
+      (swap! users assoc id updated-user)
+      {:status 200
+       :body updated-user})
+    {:status 404
+     :body {:error "User not found"}}))
+
+
+(defn delete-user
+  [{{:keys [id]} :path-params}]
+  (if (get @users id)
+    (do
+      (swap! users dissoc id)
+      {:status 204})
+    {:status 404
+     :body {:error "User not found"}}))
+
+
 (def app
   (ring/ring-handler
     (ring/router
       ["/"
-       ["" string-handler]]
+       ["" string-handler]
+       ["users" {:get get-users
+                 :post create-user}]
+       ["users/:id" {:get get-user
+                     :put update-user
+                     :delete delete-user}]]
       {:data {:muuntaja m/instance
               :middleware [muuntaja/format-middleware]}})))
 
 
 (defn start
   []
-  (jetty/run-jetty app {:port 3000 :join? false}))
+  (when-not @server  ; サーバーが起動していない場合のみ起動
+    (reset! server (jetty/run-jetty app {:port 3000 :join? false}))))
 
 
-(def server (start))
-(.stop server)
+(defn stop
+  []
+  (when @server
+    (.stop @server)
+    (reset! server nil)))
+
+
+(start)
+
+(stop)
+@server
