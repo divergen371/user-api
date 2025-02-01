b102bd7f994bf9e9ae90e474116b10b064539034
divergen371 <51857554+divergen371@users.noreply.github.com>
2025-02-01 13:38:21 +0900

    ルーティング

--- src/user_api/core.clj
+++ src/user_api/core.clj
@@ -3,20 +3,29 @@
   (:require
     [muuntaja.core :as m]
     [reitit.ring :as ring]
-    [retit.ring.middleware.muuntaja :as muuntaja]
+    [reitit.ring.middleware.muuntaja :as muuntaja]
     [ring.adapter.jetty :as jetty]))
 
 
-(defn handler
-  [request]
+(defn string-handler
+  [_]
   {:starus 200
    :headers {"Content-Type" "text/html"}
    :body "Hello!! I'm a Clojure API"})
 
 
+(def app
+  (ring/ring-handler
+    (ring/router
+      ["/"
+       ["" string-handler]]
+      {:data {:muuntaja m/instance
+              :middleware [muuntaja/format-middleware]}})))
+
+
 (defn start
   []
-  (jetty/run-jetty handler {:port 3000 :join? false}))
+  (jetty/run-jetty app {:port 3000 :join? false}))
 
 
 (def server (start))
